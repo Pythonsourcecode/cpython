7da49ed66e7088506c5e8e68053d4a00aaeebb65
Pythonsourcecode <agraham98@student.cccs.edu>
2024-08-30 13:18:42 -0600

    Add files via upload

---
+++ Graham.py Greeting Program Exercise.py
@@ -0,0 +1,16 @@
+#Amelia Graham
+#CSC10192H1 Intro to Programming (Python)
+#Programming Exercise Assignment on an English greeting of my choice and another language greeting of my choice.
+
+#English Greeting
+print("Hello, What's your name?")
+# Then type in your name.
+
+print("Hello my name is Amelia Graham it's nice to meet you!")
+
+#Irish Greeting
+def greet (greeting):
+    name = raw_input("Hi, whats your name?")
+    return greeting + name
+
+print("DEE-ah GHWIT.")
